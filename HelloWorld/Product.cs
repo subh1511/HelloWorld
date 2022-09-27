e27b6ce4613d48b553fd8d4f6d49d8de517020a3
subh1511 <1subhrajit15@gmail.com>
2022-09-27 15:18:51 +0530

    File Product Added

---
+++ HelloWorld/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+
+    }
+}
