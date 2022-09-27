0835d93dd5f38e5d841eac31ece4b4d236780a4a
subh1511 <1subhrajit15@gmail.com>
2022-09-27 15:30:54 +0530

    New Dll Added

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EMS;
 namespace HelloWorld
 {
     internal class Program
@@ -12,6 +12,13 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World! ");
             Console.WriteLine("Today is Tuesday");
+            Emp emp = new Emp
+            {
+                ID = 20,
+                Name = "Tom",
+                Salary = 40000
+            };
+            Console.WriteLine($"{emp.ID} {emp.Name} {emp.Salary}");
         }
     }
 }
